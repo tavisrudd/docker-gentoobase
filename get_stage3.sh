55c3c6e9b6a47e95912f3e3f9470c1b2d1dcada7
Tavis Rudd <tavis@damnsimple.com>
2014-06-12 11:33:33 -0700

    add caching to get_stage3.sh

--- get_stage3.sh
+++ get_stage3.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+set -e
+latest="$(curl -s http://distfiles.gentoo.org/releases/amd64/autobuilds/latest-stage3-amd64.txt | tail -n1)"
+tarball="$(basename "$latest")"
+[[ -e "$tarball" ]] || {
+    wget -O "$tarball" "http://distfiles.gentoo.org/releases/amd64/autobuilds/$latest"
+    rm -f stage3.latest.tar.bz2
+    ln -s "$tarball" stage3.latest.tar.bz2
+}
 
-latest=$(curl -s http://distfiles.gentoo.org/releases/amd64/autobuilds/latest-stage3-amd64.txt | tail -n1)
-wget http://distfiles.gentoo.org/releases/amd64/autobuilds/$latest
