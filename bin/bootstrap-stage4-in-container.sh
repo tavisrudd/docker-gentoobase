841985f3741d80f2b4f8b7a4dc58c484f5c8c3f7
Tavis Rudd <tavis@damnsimple.com>
2014-06-12 13:42:58 -0700

    cosmetic refactor

--- bin/bootstrap-stage4-in-container.sh
+++ bin/bootstrap-stage4-in-container.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-get_portage() {
+
+get_portage_snapshot() {
     [[ -e /usr/portage ]] || {
         wget -qO- http://gentoo.arcticnetwork.ca/snapshots/portage-latest.tar.xz | tar xfJ - -C /usr 
     }
@@ -7,16 +8,21 @@ get_portage() {
 salt_local() {
     salt-call -l warning --retcode-passthrough --local "$@"
 }
-get_portage && 
+
+configure_portage() {
+    mkdir -p /etc/portage/package.keywords/
+    cp /srv/salt/base/etc/portage/make.conf /etc/portage/make.conf
+    cat > /etc/portage/package.keywords/salt <<-EOF
+	>=app-admin/salt-0.17.4-r1 ~amd64
+	>=dev-python/msgpack-0.4.0 ~amd64
+	>=dev-python/pycryptopp-0.6.0 ~amd64
+	>=dev-python/pyyaml-3.10-r1 ~amd64
+	EOF
+}
+
+configure_portage &&
+get_portage_snapshot && 
 ln -sf /proc/self/fd /dev/ &&
-mkdir -p /etc/portage/package.keywords/ &&
-cp /srv/salt/base/etc/portage/make.conf /etc/portage/make.conf &&
-cat > /etc/portage/package.keywords/salt <<EOF
->=app-admin/salt-0.17.4-r1 ~amd64
->=dev-python/msgpack-0.4.0 ~amd64
->=dev-python/pycryptopp-0.6.0 ~amd64
->=dev-python/pyyaml-3.10-r1 ~amd64
-EOF
 emerge -u --usepkg --buildpkg --quiet --quiet-build salt &&
 salt_local state.sls base.salt-patches &&
 salt_local state.highstate
